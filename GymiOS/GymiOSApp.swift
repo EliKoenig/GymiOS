a3af44ce57366d4847ec162934e38dfbd4554136
EliKoenig <elifireguyfburg@gmail.com>
2025-09-11 19:22:43 -0400

    Initial Commit

---
+++ GymiOS/GymiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymiOSApp.swift
+//  GymiOS
+//
+//  Created by lending on 9/11/25.
+//
+
+import SwiftUI
+
+@main
+struct GymiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GymiOSTests/GymiOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  GymiOSTests.swift
+//  GymiOSTests
+//
+//  Created by lending on 9/11/25.
+//
+
+import Testing
+@testable import GymiOS
+
+struct GymiOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
